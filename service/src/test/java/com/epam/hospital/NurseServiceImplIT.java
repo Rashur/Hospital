3b72ac45156c478dc54e33c3ac0f7f8837b3bb6c
Dzianis <beresten03@mail.ru>
2021-11-26 16:05:27 +0300

    refactor dao and service tests

--- service/src/test/java/com/epam/hospital/NurseServiceImplIT.java
+++ service/src/test/java/com/epam/hospital/NurseServiceImplIT.java
@@ -10,9 +10,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = {"classpath*:test-db.xml", "classpath*:service-context-test.xml"})
@@ -22,50 +25,63 @@ class NurseServiceImplIT {
     private static final Logger log = LogManager.getLogger(NurseServiceImplIT.class);
     private final NurseServiceImpl nurseService;
 
-    public NurseServiceImplIT(@Autowired NurseServiceImpl nurseService) {
+    public NurseServiceImplIT(@Autowired final NurseServiceImpl nurseService) {
         this.nurseService = nurseService;
     }
-    //TODO refactor test logic
+
     @Test
-    void findAll() {
-        log.info("IN NurseServiceImplIT execute test findAll()");
+    void shouldReturnAllNurses() {
+        log.info("Execute nurseService test shouldReturnAllNurses()");
         assertNotNull(nurseService);
-        assertNotNull(nurseService.findAll());
+        List<Nurse> allNurses = nurseService.findAll();
+        assertNotNull(allNurses);
+        assertEquals(3, allNurses.size());
     }
 
     @Test
-    void create() {
-        log.info("IN NurseServiceImplIT execute test create()");
+    void shouldCreateNurseSuccessfully() {
+        log.info("Execute nurseService test shouldCreateNurseSuccessfully()");
         assertNotNull(nurseService);
-        int nursesSizeBefore = nurseService.findAll().size();
-        Nurse nurse = new Nurse(5,"Tatsiana", "Levchuk");
-        Integer nurseId = nurseService.create(nurse);
+        Nurse expectedNurse = new Nurse(null,"Tatsiana", "Levchuk");
+        Integer nurseId = nurseService.create(expectedNurse);
         assertNotNull(nurseId);
-        assertEquals(nursesSizeBefore, nurseService.findAll().size()-1);
+        expectedNurse.setId(nurseId);
+        Optional<Nurse> actualNurse = nurseService.findById(nurseId);
+        assertTrue(actualNurse.isPresent());
+        assertEquals(expectedNurse, actualNurse.get());
     }
 
     @Test
-    void update() {
-        log.info("IN NurseServiceImplIT execute test update()");
+    void shouldFindNurseById() {
+        log.info("Execute nurseService test shouldFindNurseById()");
         assertNotNull(nurseService);
-        Nurse foundNurse = nurseService.findById(1).get();
-        Nurse nurse = new Nurse(1,"Ekaterina", "Ivanova");
-        nurseService.update(nurse);
-        Nurse updatedNurse = nurseService.findById(1).get();
-        assertNotEquals(foundNurse, updatedNurse);
+        Nurse expectedNurse = new Nurse(1, "Eigenia", "Liashuk");
+        Optional<Nurse> actualNurse = nurseService.findById(1);
+        assertTrue(actualNurse.isPresent());
+        assertEquals(expectedNurse, actualNurse.get());
     }
 
     @Test
-    void delete() {
-        log.info("IN NurseServiceImplIT execute test delete()");
+    void shouldUpdateNurse() {
+        log.info("Execute nurseService test shouldUpdateNurse()");
         assertNotNull(nurseService);
-        //TODO realize with join table patient
+        Nurse expectedNurse = nurseService.findById(1).get();
+        Nurse actualNurse = new Nurse(1,"Ekaterina", "Ivanova");
+        expectedNurse.setFirstName("Ekaterina");
+        expectedNurse.setLastName("Ivanova");
+        nurseService.update(actualNurse);
+        actualNurse = nurseService.findById(1).get();
+        assertEquals(expectedNurse, actualNurse);
     }
 
     @Test
-    void findById() {
-        log.info("IN NurseServiceImplIT execute test findById()");
+    void shouldDeleteNurse() {
+        log.info("Execute nurseService test shouldDeleteNurse()");
         assertNotNull(nurseService);
-        assertNotNull(nurseService.findById(1));
+        Nurse expectedNurse = nurseService.findById(1).get();
+        Integer deletedId = nurseService.delete(expectedNurse.getId());
+        Optional<Nurse> deletedNurse = nurseService.findById(deletedId);
+        assertTrue(deletedNurse.isEmpty());
     }
+
 }
